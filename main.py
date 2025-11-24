6316a5da11d49c1dba6e2f8d4cdecf64e0b20749
madsci69 <ambiguous.agent@gmail.com>
2025-11-23 21:02:11 -0500

    added functionality

--- main.py
+++ main.py
@@ -1,4 +1,41 @@
 import sys
+import os
+import shutil
+import subprocess
+from sys import executable
+
+shellFunction = {'exit': exit_shell, 'echo': echo, 'type': typeShell, 'pwd': present_directory}
+
+def find_function(function):
+    path = os.environ.get('PATH')
+    pathList = path.split(os.pathsep)
+    searched_path = set()
+    foundPath = []
+    for path in pathList:
+        for root, dirnames, filenames in os.walk(path, followlinks=False):
+            if root in searched_path:
+                pass
+            else:
+                if function in filenames:
+                    if os.access(os.path.join(root, function), os.X_OK):
+                        foundPath.append(os.path.join(root, function))
+                        return foundPath[0]
+                #searched_path.add(root)
+    return False
+    
+def present_directory():
+    print(os.getcwd())
+    
+def typeShell(userInput):
+    typeInput = "".join(userInput)
+    if typeInput in shellFunction:
+        print(f"{typeInput} is a shell builtin")
+    else:
+        foundPath = find_function(typeInput)
+        if foundPath:
+            print(f"{typeInput} is {foundPath}")
+        else:
+            print(f"{typeInput}: not found")
 
 def exitShell(status):
     status = "".join(status)
@@ -6,26 +43,53 @@ def exitShell(status):
         return True
     else:
         return False
+        
 def echo(input):
     inputJoined = " ".join(input)
     print(inputJoined)
+    
 
 def shellInput(input):
-    function = {'exit': exitShell, 'echo': echo}
-    separated_input = input.split()
-    if separated_input[0] in function:
-        return function[separated_input[0]](separated_input[1:])
+    if not userInput:
+        return None
+    separated_input = userInput.split()
+
+    function_name = separated_input[0]
+    args = separated_input[1:]
+    if separated_input[0] in shellFunction:
+        if not args:
+            return shellFunction[function_name]()
+        else:
+            return shellFunction[function_name](args)
+
+    foundExecutablePath = find_function(function_name)
+    if foundExecutablePath and len(args) > 0:
+        run_function_with_args(foundExecutablePath, args)
+        return None
+    elif foundExecutablePath and len(args) == 0:
+       run_function_no_args(foundExecutablePath)
+       return None
     else:
-        print(f"{input}: command not found")
+        print(f"{userInput}: command not found")
+        return None
+        
+def run_function_with_args(path, args):
+    head, tail = os.path.split(path)
+    local_function = [tail]
+    local_function.extend(args)
+    subprocess.run(local_function)
+    
+def run_function_no_args(path):
+    head, tail = os.path.split(path)
+    local_function = [tail]
+    subprocess.run(local_function)
+    
 def main():
-    # TODO: Uncomment the code below to pass the first stage
     while True:
         sys.stdout.write("$ ")
         userInput = input()
         returned_input = shellInput(userInput)
 
-        if returned_input:
-            break
 
 
 
